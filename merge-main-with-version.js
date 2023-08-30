6903b3bebe4589013c789f7d2cab689395187ab6
Philip Roberts <phil@xchart.com>
2023-08-30 11:33:29 +0100

    Update

--- merge-main-with-version.js
+++ merge-main-with-version.js
@@ -1,20 +1,26 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 // Using git and child_process.exec_sync determine the version in package.json on the main branch
 const { execSync } = require("child_process");
 const execSyncString = (cmd) => execSync(cmd).toString().trim();
 
+try {
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
 const pwd = execSyncString("pwd");
+console.log(pwd);
 
 const mainBranchPackageJsonContents = execSyncString(
   "git show main:package.json"
 );
 const version = JSON.parse(mainBranchPackageJsonContents).version;
 console.log(version);
-
-// Find the commit sha that had the version as the text in the commit message
-const commitSha = execSyncString(
-  `git log main --grep="^${version}$" --format="%H" -1`
-);
-console.log("Sha:", commitSha);
 
 // Get the current version on our feature branch
 const currentVersion = require(pwd + "/package.json").version;
@@ -32,10 +38,22 @@ const semver = require("semver");
 const bump = semver.diff(mergeBaseVersion, currentVersion);
 console.log("Bump:", bump);
 
-// Cherry-pick the version bump from main
-execSync(`git cherry-pick ${commitSha} --strategy-option theirs`);
+// overwrite with main version
+execSync(`npm version ${version} --no-git-tag-version`);
+execSync(`git add package.json`);
+try {
+  execSync(`git add package-lock.json`);
+} catch (e) {}
+execSync(`git commit -m "revert version to ${version}"`);
 
 execSync("git merge main");
 
 // Update the version in package.json
-execSync(`npm version ${bump}`);
+execSync(`npm version ${bump} --no-git-tag-version`);
+execSync(`git add package.json`);
+try {
+  execSync(`git add package-lock.json`);
+} catch (e) {}
+execSync(`git commit -m "update version to ${version}"`);
+execSync(`git log`);
+execSync(`git diff main`);
